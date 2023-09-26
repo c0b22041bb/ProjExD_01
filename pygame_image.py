e63b81b351a0a7428f68092b22d1f65f2c0ef217
Aoto <c0b22041bb@edu.teu.ac.jp>
2023-09-26 17:26:46 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,24 @@ def main():
     cg_img = pg.transform.flip(cg_img,True,False)
     cg_img2 = pg.transform.rotozoom(cg_img,10,1.0)
     cg_imgs = [cg_img,cg_img2]
+    bg_imgs = pg.transform.flip(bg_img,True,False)
     tmr = 0
 
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        x = tmr
+
+        x = tmr % 3200
+        if tmr % 100 <= 50:
+            num = 0
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
-        screen.blit(cg_imgs[tmr%2],[300,200])
+        screen.blit(bg_img,[-x+3200,0])
+        screen.blit(bg_imgs,[1600-x,0])
+        screen.blit(cg_imgs[num],[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1       
+        clock.tick(500)
 
 if __name__ == "__main__":
     pg.init()
